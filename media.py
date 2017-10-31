a10e08994a6b949c2fb8562dc08297977e18aab8
aleighton <a.leighton@protonmail.com>
2017-10-31 09:58:16 +0800

    added comments and reformatted code to comply with PEP8 conventions

--- media.py
+++ media.py
@@ -1,7 +1,22 @@
 import webbrowser
 
+
 class Movie():
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """Constructor for Class Movie
+    Takes 5 arguments:
+    self
+    movie_title --- supplied as a string
+    movie_storyline --- supplied as a string
+    poster_image --- should be supplied as a url
+    trailer_youtube --- should be supplied as a url
+
+    show_trailer() is a class method of Movie.
+    The youtube url supplied to the constructor
+    is passed to the method and a browser window is opened with the link.
+
+    """
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
